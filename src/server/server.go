c76ff28c1a93d48ded95eab6a2a6bf00454505d4
Radim Hopp <rhopp@redhat.com>
2019-10-11 08:26:29 +0200

    Homework ver.1
    Signed-off-by: Radim Hopp <rhopp@redhat.com>

--- src/server/server.go
+++ src/server/server.go
@@ -18,7 +18,27 @@ func handleConnection(c net.Conn) {
 	c.Write([]byte("You said: " + netData))
 }
 
+func handleConnectionRoutine(c chan net.Conn) {
+	for connection := range c {
+		handleConnection(connection)
+	}
+}
+
 func main() {
+
+	//create channel
+	channel := make(chan net.Conn)
+
+	// close channel on exit
+	defer func(c chan net.Conn) {
+		close(c)
+	}(channel)
+
+	// create 4 goroutines
+	for i := 1; i < 5; i++ {
+		go handleConnectionRoutine(channel)
+	}
+
 	l, err := net.Listen("tcp4", ":5002")
 	if err != nil {
 		fmt.Println(err)
@@ -32,6 +52,7 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		go handleConnection(c)
+		// send accepted connection to channel
+		channel <- c
 	}
 }
